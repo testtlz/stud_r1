fc579d0e4a1febc5dbd0be08f3499d3f61f673f0
Grzegorz Ambrożewicz <gambrozewicz@hc.local>
2020-10-17 17:54:29 +0200

    df

--- r1.R
+++ r1.R
@@ -12,3 +12,26 @@ weatherText<-content(getWeather,"text")
 weatherJson<-fromJSON(weatherText,flatten = TRUE)
 weatherDF<- as.data.frame(weatherJson)
 View(weatherDF)
+
+
+vec = c(-1,0.2,1,2)
+vec<- as.logical(vec)
+vec
+
+wektor1<-seq(1,10,1)
+
+wektor2<-2
+
+wektor1/wektor2
+
+wektor1%/%wektor2
+
+wektor1%%wektor2
+
+plecf<- factor(c("mezczyzna","kobieta","mezczyzna","kobieta","kobieta"),levels =c("mezczyzna","kobieta") )
+
+unclass(plecf)
+
+df<- data.frame(index=c(1,2,3),imie=c("jan","ala","bartek"),plec=factor(c("mezczyzna","kobieta","mezczyzna")) )
+
+View(df)
