d372b64a4a1328097e6e1b79351dc0b04a5d4e72
Grzegorz Ambrożewicz <gambrozewicz@hc.local>
2020-10-17 16:59:09 +0200

    weatherDF

--- r1.R
+++ r1.R
@@ -1,1 +1,14 @@
 print("hello")
+
+#install.packages("httr")
+#install.packages("jsonlite")
+library(httr)
+library(jsonlite)
+#install.packages()
+
+endpoint <- "http://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=ccd2c7f8b414cadf0c4383ce0a541dc2"
+getWeather <- httr::GET(endpoint)
+weatherText<-content(getWeather,"text")
+weatherJson<-fromJSON(weatherText,flatten = TRUE)
+weatherDF<- as.data.frame(weatherJson)
+View(weatherDF)
